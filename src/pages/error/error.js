d3cd3b44f8381e650195706b60588a11351fb108
Nikita Kedrovskii <nikita.kedrovskii@cleverlance.com>
2023-09-25 10:16:23 +0200

    asdasd

---
+++ src/pages/error/error.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+function Error() {
+
+    return (
+        <main>
+        </main>
+    );
+}
+
+export default Error;
