89201b68c81fbd9b6b0dc21b9f2fcd367135de22
Nikita Kedrovskii <nikita.kedrovskii@cleverlance.com>
2023-10-03 10:15:36 +0200

    asdasd

---
+++ src/pages/timer/TimerProvider.js
@@ -0,0 +1,31 @@
+// TimerContext.js
+
+import React, { createContext, useState, useEffect } from 'react';
+
+const TimerContext = createContext();
+
+const TimerProvider = ({ children }) => {
+    const [seconds, setSeconds] = useState(90);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setSeconds(prevSeconds => {
+                if (prevSeconds === 1) {
+                    clearInterval(interval);
+                    return 0;
+                }
+                return prevSeconds - 1;
+            });
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <TimerContext.Provider value={seconds}>
+            {children}
+        </TimerContext.Provider>
+    );
+};
+
+export { TimerProvider, TimerContext };
