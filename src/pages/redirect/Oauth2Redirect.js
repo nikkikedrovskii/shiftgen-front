f416fbeca9ec13048b5f00a9b25adcbd6d610251
Nikita Kedrovskii <nikita.kedrovskii@cleverlance.com>
2023-10-18 14:32:02 +0200

    add warning

--- src/pages/redirect/Oauth2Redirect.js
+++ src/pages/redirect/Oauth2Redirect.js
@@ -17,10 +17,11 @@ function RedirectPage() {
                 }, 90000);
 
                 const responsePromise = await fetch('http://shiftgen-project-env.eba-bjpjpizj.eu-north-1.elasticbeanstalk.com/strategy/generate', {
+                    mode: 'no-cors',
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${localStorage.getItem('token')}`,
+                        'Authorization': `Bearer ${localStorage.getItem('token')}`
                     },
                     body: JSON.stringify({
                         analytics: localStorage.getItem("data")
