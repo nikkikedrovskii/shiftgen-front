91c2970e27b5e189b523e1f9a7252923b327c949
Nikita Kedrovskii <nikita.kedrovskii@cleverlance.com>
2023-10-18 08:33:55 +0200

    add warning

--- src/pages/timer/TimerProvider.js
+++ src/pages/timer/TimerProvider.js
@@ -5,7 +5,7 @@ import React, { createContext, useState, useEffect } from 'react';
 const TimerContext = createContext();
 
 const TimerProvider = ({ children }) => {
-    const [seconds, setSeconds] = useState(90);
+    const [seconds, setSeconds] = useState(120);
 
     useEffect(() => {
         const interval = setInterval(() => {
