69d7aacb06be1b5f2d91891918292092d78896ac
Nikita Kedrovskii <nikita.kedrovskii@cleverlance.com>
2023-10-20 10:47:19 +0200

    change help

--- src/pages/redirect/Oauth2Redirect.js
+++ src/pages/redirect/Oauth2Redirect.js
@@ -131,7 +131,7 @@ function RedirectPage() {
             fetchGeneratePlan()
         }
 
-      //  navigate('/conditions');
+        navigate('/conditions');
     }, [navigate, location.search]);
 
     return (
